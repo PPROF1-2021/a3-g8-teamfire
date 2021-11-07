957eaba9dc24d17a381fa4e07532d302b2161bca
Noelia Cabral <90397854+dakoo096@users.noreply.github.com>
2021-11-07 00:25:40 -0300

    se agregan modificaciones en archivos js y css

--- frontend/css/js/main.js
+++ frontend/css/js/main.js
@@ -43,22 +43,22 @@
   /**
    * Navbar links active state on scroll
    */
-  let navbarlinks = select('#navbar .scrollto', true)
-  const navbarlinksActive = () => {
-    let position = window.scrollY + 200
-    navbarlinks.forEach(navbarlink => {
-      if (!navbarlink.hash) return
-      let section = select(navbarlink.hash)
-      if (!section) return
-      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-        navbarlink.classList.add('active')
-      } else {
-        navbarlink.classList.remove('active')
-      }
-    })
-  }
-  window.addEventListener('load', navbarlinksActive)
-  onscroll(document, navbarlinksActive)
+   let navbarlinks = select('#navbar .scrollto', true)
+   const navbarlinksActive = () => {
+     let position = window.scrollY + 200
+     navbarlinks.forEach(navbarlink => {
+       if (!navbarlink.hash) return
+       let section = select(navbarlink.hash)
+       if (!section) return
+       if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+         navbarlink.classList.add('active')
+       } else {
+         navbarlink.classList.remove('active')
+       }
+     })
+   }
+   window.addEventListener('load', navbarlinksActive)
+   onscroll(document, navbarlinksActive)
   /**
    * Scrolls to an element with header offset
    */
